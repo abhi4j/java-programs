d28664a6899c3321a4938f8351a3c4f067ba88f9
abhi4j <abhi559416@yahoo.com>
2014-04-07 21:38:54 +0530

    inch to cm conversion

---
+++ Constant.java
@@ -0,0 +1,11 @@
+public class Constant
+{
+ public static void main()
+    {
+	final cm_per_intch=2.54;
+	double papperwidth= 12;
+	double paperheight= 15;
+	System.out.println("Dimentions of the paper in centimeter are: "
+                             +paperwidth*cm_per_intch+ "*" +paperheight*cm_per_intch      );
+    }
+}

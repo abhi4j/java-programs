0a867b05cde36af131acc28ff5abd121999703ab
abhi4j <abhi559416@yahoo.com>
2014-04-09 19:00:07 +0530

    Java package

---
+++ teacher.java
@@ -0,0 +1,14 @@
+package p1;
+
+public class teacher
+{
+        int i;
+        String name[];
+	public teacher()
+        {                                           
+                String name[]={"pachghare"};
+                for(i=0;i<name.length;i++)
+                System.out.println("This Is Teacher : "+name[i]);
+        }
+
+}
